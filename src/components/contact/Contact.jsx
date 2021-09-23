ab0486dfa1f0f71372a4c62738a60a1af967a349
Manuela Rodriguez-Acosta Cassinello <mrac.in@icloud.com>
2021-09-23 13:33:51 +0200

    page contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,6 +3,9 @@ import "./contact.scss"
 export default function Contact() {
   return (
     <div className="contact" id="contact">
+      <div className="info">
+        <h1> Work inquires at mrac.in@icloud.com</h1>
+      </div>
 
     </div>
   )
