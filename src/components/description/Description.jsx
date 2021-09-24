eb8a03737cdf47576f93ebcfbaff4dee96f92ab7
Manuela Rodriguez-Acosta Cassinello <mrac.in@icloud.com>
2021-09-24 19:15:44 +0200

    worked on the homepage

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -4,15 +4,14 @@ export default function Description() {
   return (
     <div className="description" id="description">
       <div className="hey">
-        <h1>Graphic Designer & Frontend Developer</h1>
+        <h1> I design experiences.</h1>
         <h2> Combining my experience in design and background as an artist.</h2>
       </div>
 
       <div className="heyo">
-        <h3>This is Manuela Cassinello. Proactive, results-driven, empathetic and hard-working person </h3>
-        <h4> I tell stories and create compelling brand experiences.
-          My passion for typography goes hand in hand with my interest
-          in UX/UI and editorial design.
+        <h3>This is Manuela Cassinello. Graduate Frontend Developer & Graphic design student based in Barcelona</h3>
+        <h4> I like to create experiences that
+          solve complex problems and have a positive impact on users and the planet.
          </h4>
       </div>
     </div>
