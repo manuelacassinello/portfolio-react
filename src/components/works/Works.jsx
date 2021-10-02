af299cad894cb0d490be7d9422958bc7b7b003b2
Manuela Rodriguez-Acosta Cassinello <mrac.in@icloud.com>
2021-10-02 12:21:29 +0200

    worked on the work page

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -2,7 +2,23 @@ import "./works.scss"
 export default function works() {
   return (
     <div className="works" id="works">
-
+      <div className="container">
+        <div className="item">
+          <img src="https://res.cloudinary.com/manuelacass/image/upload/v1633166027/Screenshot_2021-10-02_at_11.13.11_eeosr2.png" alt=""/>
+          <h3>Taa</h3>
+          <h4>Web Application</h4>
+        </div>
+        <div className="item2">
+          <img src="https://res.cloudinary.com/manuelacass/image/upload/v1633167969/Youtube_aawc5o.jpg" alt="" />
+          <h3>La caldera - Les Corts</h3>
+          <h4>Re-Branding / Art Direction</h4>
+        </div>
+        <div className="item3">
+          <img src="https://res.cloudinary.com/manuelacass/image/upload/v1633168901/Screenshot_2021-10-02_at_12.01.25_lhr3ra.png" alt="" />
+          <h3>Bounce</h3>
+          <h4>Art Direction</h4>
+        </div>
+      </div>
     </div>
   )
 }
