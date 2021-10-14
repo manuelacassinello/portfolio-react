69f2b147551ba1e3571a135ca2a69e380c93ecda
Manuela Rodriguez-Acosta Cassinello <mrac.in@icloud.com>
2021-10-14 09:58:48 +0200

    did la caldera

--- src/components/caldera2/Caldera2.jsx
+++ src/components/caldera2/Caldera2.jsx
@@ -4,18 +4,23 @@ import "./caldera2.scss"
 export default function Caldera2() {
   return (
     <div className="caldera2" id="caldera2">
+      <h1>La Caldera - Les Corts</h1>
+      <h2>New Concept</h2>
       <div className="compo">
+
+        <div className="tt">
+          <h1>La Caldera is an artistic center characterized by being a center for artistic creation that encourages experimentation to express one’s essence, as can be seen in the variety of shows and activities. The rooms of La Caldera are old movie theaters.
+            The characteristic doors with a round window allows you to see what is inside.</h1>
+          <h2>The concept for this new corporate image comes from the concept of the windows that exist throughout the insta- llation, those round windows located in the center of the doors of
+            all rooms lets you see what happens inside. The concept of the window allowed us to represent the introspection of the artist, the organic and dynamic forms of dance, to seek complexity in everyday life.</h2>
+          <h3>From an intuitive interactive website, the resident artists and collaborators of La Caldera will generate the center’s own identity. The website allows a free choice of both
+            shape and filling, whether textures generated with Processing or a series of corporate fixed colors.</h3>
+        </div>
         <div className="imagen">
           <img src="assets/ko.png" alt="k"></img>
         </div>
-      </div>
-      <div className="tt">
-      <h1>La Caldera is an artistic center charac- terized by being a center for artistic creation that encourages experimentation to express one’s essence, as can be seen in the variety of shows and activities. The rooms of La Caldera are old movie theaters.
-      The characteristic doors with a round window allows you to see what is inside.</h1>
-      <h2>The concept for this new corporate image comes from the concept of the windows that exist throughout the insta- llation, those round windows located in the center of the doors of
-        all rooms lets you see what happens inside. The concept of the window allowed us to represent the introspection of the artist, the organic and dynamic forms of dance, to seek complexity in everyday life.</h2>
-      <h3>From an intuitive interactive website, the resident artists and collaborators of La Caldera will generate the center’s own identity. The website allows a free choice of both
-        shape and filling, whether textures generated with Processing or a series of corporate fixed colors.</h3>
+
+
       </div>
       <div className="iconabout">
         <a href="#contact">
