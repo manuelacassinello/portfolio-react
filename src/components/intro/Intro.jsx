6bf4dc90e3b66e6ca30b165c6cd6a2e88cd248d1
Manuela Rodriguez-Acosta Cassinello <mrac.in@icloud.com>
2022-01-29 20:30:46 +0100

    nfu

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,10 +5,9 @@ import "./intro.scss"
 export default function intro() {
   return (
     <div className="intro" id="intro">
-      <h1>Hey,</h1>
-      <video loop autoPlay muted className="video">
-        <source src="https://res.cloudinary.com/manuelacass/video/upload/v1633446122/video_tfkqry.mp4" type="video/mp4" />
-      </video>
+      <div className="imagen">
+        <img src="assets/990.jpg" alt="k"></img>
+      </div>
     </div>
   )
 
