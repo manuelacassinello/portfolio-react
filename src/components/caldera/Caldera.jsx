efcf1e16d0a73c3eed107ea46e71cd44b7a99c60
Manuela Rodriguez-Acosta Cassinello <mrac.in@icloud.com>
2022-02-02 20:08:07 +0100

    did some styling

--- src/components/caldera/Caldera.jsx
+++ src/components/caldera/Caldera.jsx
@@ -6,8 +6,7 @@ export default function Caldera() {
     <div className="caldera" id="caldera">
       <div className="compo">
         <div className="imagen">
-          <img src="assets/00.png" alt="k"></img>
-          <img src="assets/03.png" alt="k"></img>
+          <img src="assets/MA.png" alt="k"></img>
         </div>
       </div>
 
